62f177a7d74188764139a903447aed123d39d142
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-09-30 21:42:24 +0530

    n=int(input()) a=0 b=1 s=0 for i in range(1,100):     c=a+b     if c==n:         s=1         break     else:         a=b         b=c if s==1:     print('True') else:     print('False')

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,18 @@
+n=int(input())
+a=0
+b=1
+s=0
+for i in range(1,100):
+    c=a+b
+    if c==n:
+        s=1
+        break
+    else:
+        a=b
+        b=c
+if s==1:
+    print('True')
+else:
+    print('False')
+        
+        
