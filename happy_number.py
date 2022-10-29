c1ca5f5d564c6bffd2c01cb3f622114c101c955f
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-10-29 10:58:32 +0530

    def Happy(n):     s=0     while(n!=0):         r=n%10         s+=r*r         n=n//10         if s>9 and n==0:            n=s            s=0     if s==1 or s==7:                print('True')     else:                print('False') n=int(input()) Happy(n)

--- happy_number.py
+++ happy_number.py
@@ -1,13 +1,15 @@
+def Happy(n):
+    s=0
+    while(n!=0):
+        r=n%10
+        s+=r*r
+        n=n//10
+        if s>9 and n==0:
+           n=s
+           s=0
+    if s==1 or s==7:
+               print('True')
+    else:
+               print('False')
 n=int(input())
-s=0
-while(n!=0):
-    r=n%10
-    s+=r*r
-    n=n//10
-    if n==0 and s>9:
-        n=s
-        s=0
-if s==1 or s==7:
-    print('True')
-else:
-    print('False')
+Happy(n)
