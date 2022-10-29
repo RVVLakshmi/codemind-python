86645d071df0fd53b619d4ca9d6047998913450a
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-10-29 10:29:49 +0530

    def Add_Digits(n):     s=0     while(n!=0):         r=n%10         s+=r         n=n//10         if s>9 and n==0:             n=s             s=0     print(s) n=int(input()) Add_Digits(n)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,13 +1,12 @@
+def Add_Digits(n):
+    s=0
+    while(n!=0):
+        r=n%10
+        s+=r
+        n=n//10
+        if s>9 and n==0:
+            n=s
+            s=0
+    print(s)
 n=int(input())
-s=0
-while(n!=0):
-    r=n%10
-    s+=r
-    n=n//10
-    if s>9 and n==0:
-        n=s
-        s=0
-print(s)
-        
-              
-       
+Add_Digits(n)
