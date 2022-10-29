812cc787cf6304aeaccf743bf31c9df2209fed4c
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-10-29 10:11:12 +0530

    def Ami(m,n):     s=0     k=0     for i in range(1,m,1):         if m%i==0:             s+=i     for j in range(1,n,1):         if n%j==0:             k+=j     if s==n and k==m:         print('Amicable')     else:         print('Not Amicable') m=int(input()) n=int(input()) Ami(m,n)

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,17 +1,18 @@
+def Ami(m,n):
+    s=0
+    k=0
+    for i in range(1,m,1):
+        if m%i==0:
+            s+=i
+    for j in range(1,n,1):
+        if n%j==0:
+            k+=j 
+    if s==n and k==m:
+        print('Amicable')
+    else:
+        print('Not Amicable')
 m=int(input())
 n=int(input())
-s=0
-d=0
-for i in range(1,m,1):
-    if m%i==0:
-        s+=i
-for j in range(1,n,1):
-    if n%j==0:
-        d+=j
-if s==n and d==m:
-    print('Amicable')
-else:
-    print('Not Amicable')
-    
+Ami(m,n)
         
-        
+            
